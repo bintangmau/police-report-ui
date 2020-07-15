adcc1591a3e2052e4737dc49248b977d921b89f3
sultanrayhan32 <sultanrayhanh@gmail.com>
2020-07-15 12:57:15 +0700

    pull dev, table ui done

--- src/Components/ViewReport/index.jsx
+++ src/Components/ViewReport/index.jsx
@@ -3,11 +3,15 @@ import React from 'react'
 // STYLE
 import './style.css'
 
+// COMPONENT 
+import TableContent from './TableContent'
+
 function ViewReport () {
 
     return (
         <div className="view-report-container-02">
-            <div>VIEW REPORT</div>
+            <h1>Lihat Laporan</h1>
+            <TableContent/>
         </div>
     )
 
