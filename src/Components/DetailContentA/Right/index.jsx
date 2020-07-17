742839603b9b05e134f6111aa088eb600460ab29
sultanrayhan32 <sultanrayhanh@gmail.com>
2020-07-17 14:16:06 +0700

    pull new

--- src/Components/DetailContentA/Right/index.jsx
+++ src/Components/DetailContentA/Right/index.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 export default function Right() {
     return (
         <div className="detail-a-right">
-            right
+            
+            <div className="detail-right-box">
+
+            </div>
+
+            <div className="detail-right-box">
+
+            </div>
+
         </div>
     )
 }
--- src/helper/database/index.js
+++ src/helper/database/index.js
@@ -1,2 +1,3 @@
-export const api = 'http://localhost:2001/'
+// export const api = 'http://localhost:2001/'
 // export const api = 'http://2f194ad3d45c.ngrok.io/'
+export const api =  'http://30312006844b.ngrok.io/'
