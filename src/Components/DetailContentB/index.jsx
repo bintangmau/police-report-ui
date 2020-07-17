aca2039eef12fb7aa7bb1ec4706ce68e5c0f9cd5
bintangmau <bintangmaulanahabib@gmail.com>
2020-07-17 14:24:14 +0700

    commit

--- src/Components/DetailContentB/index.jsx
+++ src/Components/DetailContentB/index.jsx
@@ -20,7 +20,13 @@ function DetailContentA (props) {
     const [ data, setData ] = useState('')
 
     const getDetailsReportA = () => {
-        Axios.get(api + 'report/get-report-b-details/' + params)
+        Axios({
+            method: "GET",
+            url: api + 'report/get-report-b-details/' + params,
+            headers: {
+                token: localStorage.getItem('token')
+            }
+        })
         .then((res) => {
             setData(res.data)
         })
