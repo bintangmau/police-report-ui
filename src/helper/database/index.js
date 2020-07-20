e57f717f80cebdab8c07f90f72726b5ae22fc64f
sultanrayhan32 <sultanrayhanh@gmail.com>
2020-07-20 13:26:08 +0700

    l

--- src/helper/database/index.js
+++ src/helper/database/index.js
@@ -1,3 +1,3 @@
 // export const api = 'http://localhost:2001/'
 // export const api = 'http://2f194ad3d45c.ngrok.io/'
-export const api =  'http://801155870845.ngrok.io/'
+export const api =  'http://7eaf81d5a029.ngrok.io/'
